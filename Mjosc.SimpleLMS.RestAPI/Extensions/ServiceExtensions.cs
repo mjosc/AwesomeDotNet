10e26dad198f43a6bfdf6026f608d2622bbeeabc
Matt Josse <matt.m.josse@gmail.com>
2019-01-08 11:15:59 -0700

    Added Jwt authentication service

---
+++ Mjosc.SimpleLMS.RestAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Mjosc.SimpleLMS.RestAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            string keyString = configuration.GetSection("AuthenticationStrings")["JwtSecretKey"];
+            byte[] secretKey = Encoding.ASCII.GetBytes(keyString);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
