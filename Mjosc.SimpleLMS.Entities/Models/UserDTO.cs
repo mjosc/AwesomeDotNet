bdef677a85e2c918a56cbac61825a9ebfd3002ab
Matt Josse <matt.m.josse@gmail.com>
2019-01-09 18:40:43 -0700

    Initial commit

--- Mjosc.SimpleLMS.Entities/Models/UserDTO.cs
+++ Mjosc.SimpleLMS.Entities/Models/UserDTO.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+
 namespace Mjosc.SimpleLMS.Entities.Models
 {
     public class UserDTO
     {
-        public UserDTO()
-        {
-        }
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Role { get; set; }
     }
 }
